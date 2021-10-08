cc72bf4c44b9f118beeae62dce3f5769e6919ecf
TheBoringDude <iamcoderx@gmail.com>
2021-10-08 20:48:38 +0800

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  projectRoot: ".", // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
@@ -11,4 +20,4 @@ export default {
     // port: 3000,         // The port to run the dev server on.
     tailwindConfig: "./tailwind.config.js", // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
-};
+});
